9fed3eee9f192cb14285f6e8d887776e6da8fc6e
drbilalkhalid <bilalkhalid9256@gmail.com>
2026-07-17 23:52:18 +0500

    part 4 c complete

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,9 +1,9 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
-// const User = require('../models/user')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
   response.json(notes)
 })
 
@@ -21,15 +21,23 @@ notesRouter.post('/', async (request, response) => {
 
   console.log(body)
 
-  // const user = await User.findById(body.userId)
-  // console.log(user)
+  const user = await User.findById(body.user)
+
+  if (!user) {
+    return response.status(400).json({ error: 'user missing or not valid' })
+  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
+    user: user._id,
   })
 
   const savedNote = await note.save()
+
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   response.status(201).json(savedNote)
 })
 
